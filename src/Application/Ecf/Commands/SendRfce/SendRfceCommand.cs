c32052855d2376ecc07a2c6e88be58c897622070
Jorgegbeltre <Jorgegaspar3021@gmail.com>
2026-08-01 12:53:11 -0400

    feat: add SendRfceCommand for handling RFCE document submission requests

--- src/Application/Ecf/Commands/SendRfce/SendRfceCommand.cs
+++ src/Application/Ecf/Commands/SendRfce/SendRfceCommand.cs
@@ -1,8 +1,20 @@
+using System.Text.Json.Serialization;
 using MediatR;
 using EcfDgii.Client.Domain.Entities;
 using EcfDgii.Client.Shared.Common;
 
 namespace EcfDgii.Client.Application.Ecf.Commands.SendRfce
 {
-    public record SendRfceCommand(Rfce RfceModel) : IRequest<Result<RfceRecepcionResponse>>;
+    public record SendRfceCommand : IRequest<Result<RfceRecepcionResponse>>
+    {
+        public Rfce RfceModel { get; set; } = new Rfce();
+
+        public SendRfceCommand() { }
+
+        [JsonConstructor]
+        public SendRfceCommand(Rfce rfceModel)
+        {
+            RfceModel = rfceModel;
+        }
+    }
 }
